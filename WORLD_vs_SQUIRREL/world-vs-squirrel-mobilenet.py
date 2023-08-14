74f9359888e7df067dae2cdce955bf8ceb2e0455
lpari053 <lpari053@uottawa.ca>
2023-08-14 17:26:34 -0400

    world-vs-squirrel

--- WORLD_vs_SQUIRREL/world-vs-squirrel-mobilenet.py
+++ WORLD_vs_SQUIRREL/world-vs-squirrel-mobilenet.py
@@ -1,5 +1,3 @@
-#Thi code perit to train a model using the pre-trained model MobileNetV2
-
 import os
 import numpy as np
 import tensorflow as tf
@@ -9,17 +7,21 @@ from tensorflow.keras.optimizers import SGD
 from tensorflow.keras.applications import MobileNetV2
 from tensorflow.keras.layers import Dense, Flatten, Input
 from tensorflow.keras.models import Model
-from tensorflow.keras.applications.mobilenet_v2 import preprocess_input  
-    
-chemin_image=r'database/world-vs-squirell'
-image_width, image_height=224,224
-batch_size=512
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+
+image_width, image_height = 224, 224
+chemin_image = r'database/world-vs-squirell'
+
+nom_MODEL = 'WORLD_vs_SQUIRREL/world_vs_squirell_mobilenet.keras'
+batch_size = 512
+epochs = 100
+
 def dataset(train_dir, validation_dir, test_dir):
     # Data preprocessing
     train_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
     validation_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
     test_datagen = ImageDataGenerator(preprocessing_function=preprocess_input)
-
+    
     train_generator = train_datagen.flow_from_directory(
         train_dir,
         target_size=(image_width, image_height),
@@ -27,7 +29,7 @@ def dataset(train_dir, validation_dir, test_dir):
         class_mode='categorical',
         shuffle=True
     )
-
+    
     validation_generator = validation_datagen.flow_from_directory(
         validation_dir,
         target_size=(image_width, image_height),
@@ -35,7 +37,7 @@ def dataset(train_dir, validation_dir, test_dir):
         class_mode='categorical',
         shuffle=True
     )
-
+    
     test_generator = test_datagen.flow_from_directory(
         test_dir,
         target_size=(image_width, image_height),
@@ -43,27 +45,56 @@ def dataset(train_dir, validation_dir, test_dir):
         class_mode='categorical',
         shuffle=True
     )
-
+    
     return train_generator, validation_generator, test_generator
-    
-    
+
+
+
+new_learning_rate=0.001
+# Load pre-trained MobileNetV2 model
+base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+# Freeze the base model's weights
+base_model.trainable = False
+
+# Create a custom model by adding additional layers on top of the base model
+
+x = base_model.output
+x = Flatten()(x)
+x = Dense(500, activation='relu')(x)
+outputs = Dense(2, activation='softmax')(x)  # Replace 2 with the number of classes in your task
+
+model = Model(inputs=base_model.input, outputs=outputs)
+
+# Compile the model
+model.compile(optimizer=SGD(learning_rate=new_learning_rate), loss='categorical_crossentropy', metrics=['accuracy'])
+
 train_generator, validation_generator, test_dataset = dataset(
     os.path.join(chemin_image, 'train'),
     os.path.join(chemin_image, 'validation'),
     os.path.join(chemin_image, 'test')
 )
 
-nom_MODEL = 'model/world_vs_squirell_mobilenet/world_vs_squirell_mobilenet.keras'
 
-# Load the best model from the checkpoint for evaluation
-model = keras.models.load_model(nom_MODEL)
+early_stopping = tf.keras.callbacks.EarlyStopping(monitor='accuracy', patience=5)
+    
+callbacks = [
+    keras.callbacks.ModelCheckpoint(
+        filepath=nom_MODEL,    # Save the best model based on the monitor
+        save_best_only=True,
+        monitor="val_accuracy"
+    ),
+    early_stopping                
+]
 
-# Evaluate the model on the test data
-train_acc = model.evaluate(train_generator)[1]
-print(f"Train accuracy: {train_acc:.5f} \n")
+# Train the model
+model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.n // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.n // batch_size,
+    callbacks=callbacks
+)
 
-val_acc = model.evaluate(validation_generator)[1]
-print(f"Val accuracy: {val_acc:.5f} \n")
 
-test_acc = model.evaluate(test_dataset)[1]
-print(f"Test accuracy: {test_acc:.5f} \n")
--- WORLD_vs_SQUIRREL/world-vs-squirrel.py
+++ WORLD_vs_SQUIRREL/world-vs-squirrel.py
@@ -1,3 +1,11 @@
+#This code permit to train a model using the pre-trained model MobileNetV2. 
+#This is a binar classification OTHER vs SQUIRREL with 3 dataset train validation test. 
+#This code work well with the repartition 25000 pictures to train an 10000 pictures for the validation and test dataset
+#In the database the other folder contains images comming from https://www.kaggle.com/datasets/hsankesara/flickr-image-dataset
+#In the database the squirrel folder contains north-east american squirrel gray-black-other###
+
+
+
 import os
 import numpy as np
 import tensorflow as tf
@@ -11,7 +19,7 @@ image_width, image_height = 224, 224
 
 chemin_image=r'database/world-vs-squirell'
 os.makedirs('model/world-vs-squirell',exist_ok=True)
-nom_MODEL='model/world-vs-squirell/world_vs_squirell.keras'
+nom_MODEL='WORLD_vs_SQUIRREL/world_vs_squirell.keras'
 # Define hyperparameter
 batch_size = 512
 epochs = 50
@@ -48,98 +56,111 @@ def dataset(train_dir,validation_dir,test_dir):
            )
 
 
-def modelie(new_learning_rate=0.01):
+new_learning_rate=0.01
 
-    data_augmentation = keras.Sequential(
-     [
-      keras.layers.Input((image_width, image_height,3)),
-     keras.layers.RandomFlip("horizontal"),
-     keras.layers.RandomRotation(0.1),
-     keras.layers.RandomZoom(0.2),
-     ]
-    )
-        
-    inputs=keras.layers.Input((image_width, image_height,3)),
-
-    x=data_augmentation(inputs)
-
-    x= keras.layers.Conv2D(8, (3, 3), activation='relu', padding='same',input_shape=(image_width, image_height, 3))(x)
-    x= keras.layers.MaxPooling2D((2, 2))(x)
-
-    x= keras.layers.Conv2D(16, (3, 3), padding='same',activation='relu')(x)
-    x= keras.layers.MaxPooling2D((2, 2))(x)
-    x= keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu')(x)
-    x= keras.layers.MaxPooling2D((2, 2))(x)
-    x= keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same')(x)
-    x= keras.layers.MaxPooling2D((2, 2))(x)
-
-    x= keras.layers.Conv2D(128, (3, 3), padding='same',activation='relu')(x)
-    x= keras.layers.MaxPooling2D((2, 2))(x)
-    x= keras.layers.Conv2D(256, (3, 3), padding='same', activation='relu')(x)
-    x= keras.layers.MaxPooling2D((2, 2))(x)
-    x= keras.layers.Conv2D(256, (3, 3), padding='same', activation='relu')(x)
-    x= keras.layers.MaxPooling2D((2, 2))(x)
-
-
-    x= keras.layers.Flatten()(x)
-
-    x= keras.layers.Dense(256, activation='relu')(x)
-
-    x= keras.layers.Dense(128, activation='relu')(x)
-    
-    x= keras.layers.Dense(64, activation='relu')(x)
-
-    x= keras.layers.Dense(32, activation='relu')(x)
-
-    x= keras.layers.Dense(16, activation='relu')(x)
-
-    x= keras.layers.Dense(8, activation='relu')(x)
-
-    outputs= keras.layers.Dense(1, activation='sigmoid')(x)
-    
-    
-    model = keras.Model(inputs=inputs, outputs=outputs)
-        
-    
-    # Compile the modele
-    model.compile(optimizer="adam",
-                  loss='binary_crossentropy',
-                  metrics=['accuracy'])
-
-    return(model)
-
-
-
-def training():
-    
-    model=modelie()
-    
-    # Load and preprocess datasets
-    train_generator,validation_generator,test_dataset=dataset( 
-        os.path.join(chemin_image,'train'),
-        os.path.join(chemin_image,'validation'),
-        os.path.join(chemin_image,'test')
-        )
-    
-    # Define early stopping callback to prevent overfitting
-    early_stopping = EarlyStopping(monitor='accuracy', patience=5)   
-        
-        
-    callbacks = [
-        keras.callbacks.ModelCheckpoint(
-            filepath=nom_MODEL,           #enregistrement du meilleure modele selon le monitor
-            save_best_only=True,
-            monitor="val_accuracy"),early_stopping               
+data_augmentation = keras.Sequential(
+    [
+    keras.layers.Input((image_width, image_height,3)),
+    keras.layers.RandomFlip("horizontal"),
+    keras.layers.RandomRotation(0.1),
+    keras.layers.RandomZoom(0.2),
     ]
+)
+    
+inputs=keras.layers.Input((image_width, image_height,3)),
 
-       # Train the model
-    model.fit(
-        train_generator,
-        steps_per_epoch=train_generator.n // batch_size,
-        epochs=epochs,
-        validation_data=validation_generator,
-        validation_steps=validation_generator.n // batch_size,
-        callbacks=callbacks)
+x=data_augmentation(inputs)
+
+x= keras.layers.Conv2D(8, (3, 3), activation='relu', padding='same',input_shape=(image_width, image_height, 3))(x)
+x= keras.layers.MaxPooling2D((2, 2))(x)
+
+x= keras.layers.Conv2D(16, (3, 3), padding='same',activation='relu')(x)
+x= keras.layers.MaxPooling2D((2, 2))(x)
+x= keras.layers.Conv2D(32, (3, 3), padding='same', activation='relu')(x)
+x= keras.layers.MaxPooling2D((2, 2))(x)
+x= keras.layers.Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+x= keras.layers.MaxPooling2D((2, 2))(x)
+
+x= keras.layers.Conv2D(128, (3, 3), padding='same',activation='relu')(x)
+x= keras.layers.MaxPooling2D((2, 2))(x)
+x= keras.layers.Conv2D(256, (3, 3), padding='same', activation='relu')(x)
+x= keras.layers.MaxPooling2D((2, 2))(x)
+x= keras.layers.Conv2D(256, (3, 3), padding='same', activation='relu')(x)
+x= keras.layers.MaxPooling2D((2, 2))(x)
+
+
+x= keras.layers.Flatten()(x)
+
+x= keras.layers.Dense(256, activation='relu')(x)
+
+x= keras.layers.Dense(128, activation='relu')(x)
+
+x= keras.layers.Dense(64, activation='relu')(x)
+
+x= keras.layers.Dense(32, activation='relu')(x)
+
+x= keras.layers.Dense(16, activation='relu')(x)
+
+x= keras.layers.Dense(8, activation='relu')(x)
+
+outputs= keras.layers.Dense(1, activation='sigmoid')(x)
+
+
+model = keras.Model(inputs=inputs, outputs=outputs)
     
 
-training()
+# Compile the modele
+model.compile(optimizer="adam",
+                loss='binary_crossentropy',
+                metrics=['accuracy'])
+
+
+
+# Load and preprocess datasets
+train_generator,validation_generator,test_dataset=dataset( 
+    os.path.join(chemin_image,'train'),
+    os.path.join(chemin_image,'validation'),
+    os.path.join(chemin_image,'test')
+    )
+
+# Define early stopping callback to prevent overfitting
+early_stopping = EarlyStopping(monitor='accuracy', patience=5)   
+    
+    
+callbacks = [
+    keras.callbacks.ModelCheckpoint(
+        filepath=nom_MODEL,           #enregistrement du meilleure modele selon le monitor
+        save_best_only=True,
+        monitor="val_accuracy"),early_stopping               
+]
+
+    # Train the model
+model.fit(
+    train_generator,
+    steps_per_epoch=train_generator.n // batch_size,
+    epochs=epochs,
+    validation_data=validation_generator,
+    validation_steps=validation_generator.n // batch_size,
+    callbacks=callbacks)
+    
+
+
+# Load the best model from the checkpoint for evaluation
+best_model = keras.models.load_model(nom_MODEL)
+
+# Load and preprocess datasets
+train_generator,validation_generator,test_dataset=dataset( 
+    os.path.join(chemin_image,'train'),
+    os.path.join(chemin_image,'validation'),
+    os.path.join(chemin_image,'test')
+    )
+
+# Evaluate the model on the test data
+train_acc = best_model.evaluate(train_generator)[1]
+print(f"Train accuracy: {train_acc:.5f} \n")
+
+val_acc = best_model.evaluate(validation_generator)[1]
+print(f"Val accuracy: {val_acc:.5f} \n")
+
+test_acc = best_model.evaluate(test_dataset)[1]
+print(f"Test accuracy: {test_acc:.5f} \n")
